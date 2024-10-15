b8d971bef52871034389b63b6279347f3c8b7ad3
Huyanhai <810839700@qq.com>
2024-10-15 10:52:06 +0800

    feat: rtc

--- src/ws/ws.gateway.ts
+++ src/ws/ws.gateway.ts
@@ -45,15 +45,73 @@ export class WSGateway implements OnGatewayInit, OnGatewayDisconnect {
     if (this.users.has(client.id)) {
       this.users.delete(client.id);
     }
+    if (this.users.size === 0) {
+      this.messages = [];
+    }
     this.server.emit('server', this.users.size);
     this.server.emit('leave', client.id);
   }
 
+  // 发送消息
   @SubscribeMessage('sendMessage')
   sendMessage(@MessageBody() body: TMessage) {
     if (body?.name) {
       this.messages.push(body);
     }
     this.server.emit('message', this.messages);
+  }
+
+  // 请求通话
+  @SubscribeMessage('call')
+  call(
+    @MessageBody()
+    body: {
+      // 远端id
+      remoteUserId: string;
+      // 本地id
+      localUserId: string;
+    },
+  ) {
+    console.log('call', body);
+
+    this.server.to(body.remoteUserId).emit('call', body);
+  }
+
+  // 允许视频通话
+  @SubscribeMessage('acceptCall')
+  acceptCall(@MessageBody() body: { roomId: string }) {
+    this.server.to(body.roomId).emit('acceptCall');
+  }
+
+  // 处理offer
+  @SubscribeMessage('offer')
+  offer(
+    @MessageBody() body: { offer: RTCSessionDescriptionInit; roomId: string },
+  ) {
+    this.server.to(body.roomId).emit('offer', body.offer);
+  }
+
+  // 处理answer
+  @SubscribeMessage('answer')
+  answer(
+    @MessageBody() body: { answer: RTCSessionDescriptionInit; roomId: string },
+  ) {
+    this.server.to(body.roomId).emit('answer', body.answer);
+  }
+
+  // 传递视频流
+  @SubscribeMessage('video')
+  video(
+    @MessageBody()
+    body: {
+      type: string;
+      remoteUserId: string;
+      candidate: RTCIceCandidate;
+    },
+  ) {
+    this.server.to(body.remoteUserId).emit('video', {
+      type: body.type,
+      candidate: body.candidate,
+    });
   }
 }
